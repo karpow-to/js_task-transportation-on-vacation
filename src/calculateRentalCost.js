3c0c949472f0c563834a30f97c19dec9ee6f165b
karpow-to <tomekkarpow@gmail.com>
2025-04-09 20:49:56 +0200

    Fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -12,7 +12,7 @@ function calculateRentalCost(days) {
     return days * basicPrice - longTermDiscount;
   } else if (days >= 3) {
     return days * basicPrice - shortTermDiscount;
-  } else {
+  } else if(days < 3) {
     return days * basicPrice;
   }
 }
